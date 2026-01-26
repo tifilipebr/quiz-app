a70b4b95dcaebcb3875fd071338701886651a371
Filipe Lacerda <tifilipebr@gmail.com>
2026-01-26 17:16:46 -0300

    feat: add initial screen

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -3,8 +3,8 @@ import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Quiz App" },
+    { name: "description", content: "Quiz App to pratic state manage using react!" },
   ];
 }
 
