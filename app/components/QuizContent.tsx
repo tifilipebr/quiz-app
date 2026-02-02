c7be1fa1b0920f4250cd6c4ec86bd59618bbdc05
Filipe Lacerda <tifilipebr@gmail.com>
2026-02-02 17:44:40 -0300

    fix: path settings

--- app/components/QuizContent.tsx
+++
@@ -1,9 +0,0 @@
-import type { FC, JSX, ReactNode } from "react";
-
-export const QuizContent: FC<{ children: ReactNode }> = ({ children }) => {
-  return (
-    <section className="flex flex-col justify-start p-6 w-md min-h-fit gap-10">
-      {children }
-    </section>
-  );
-};
